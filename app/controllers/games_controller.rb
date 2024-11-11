03670a50329fb5911231616022e76715f1d8e309
Julie Simon <sim.ju@live.fr>
2024-11-11 18:21:48 +0100

    exo letters ok

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+require 'json'
+require 'open-uri'
+
+
+class GamesController < ApplicationController
+
+  def new
+    @alphabet = ('a'..'z').to_a
+    @letters = @alphabet.sample(10)
+  end
+
+  def score
+    @available_letters = params[:grid].split
+    url = "https://dictionary.lewagon.com/#{params[:word]}"
+    @word_serialized = URI.parse(url).read
+    @word = JSON.parse(@word_serialized)
+    @input = params[:word].split('')
+
+    @included = @input.all? do |element|
+      @available_letters.include?(element)
+    end
+
+    if @word["found"] == true
+      @answer = "You won #{@word['length']}, '#{params[:word]}' exists !"
+    else
+      @answer = 'Nul!'
+    end
+  end
+end
